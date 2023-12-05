3fd2a3cf320a33796f3d0137d5e261a7477a1d6b
kishiyamat (Takeshi Kishiyama) <kishiyama.t@gmail.com>
2023-12-06 00:50:40 +0900

    Update app.py 3.5 -> gpt-4

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # Set a default model
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = "gpt-4"
 
 # Initialize chat history
 if "messages" not in st.session_state:
